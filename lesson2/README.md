a4556caf295d71129242c9786df9b2fa37f49e0a
antwanvdm <antwanvdm@gmail.com>
2026-02-26 14:42:03 +0100

    Les 2

--- lesson2/README.md
+++ lesson2/README.md
@@ -1,7 +1,8 @@
 # PRG03 Les 2
 
-- [Bekijk het resultaat van de 'hands-on' oefening uit de les van les 2](./hands-on/).
-  Heb je deze basics onder de knie? Zo niet, blijf dan oefenen met deze materialen.
+- Bekijk het resultaat van de ['hands-on'](./hands-on/) en ['extra'](./extra/) oefeningen 
+  uit de les van les 2. Heb je deze basics onder de knie? Zo niet, blijf dan oefenen met
+  deze materialen.
 - Maak de opdrachten uit de mappen "Functions & Objects" & "DOM" in de JetBrains
   EDU-course in PhpStorm als voorbereiding op de volgende les. **Let op**: Lever je
   bewijs voor de volgende les in op Brightspace als voorwaardelijkheid van deze module!
---
+++ lesson2/extra/js/main.js
@@ -0,0 +1,50 @@
+function showDate() {
+  const date = new Date();
+  const month = date.toLocaleString('nl-NL', {month: 'long'});
+  console.log(`${date.getDate()} ${month} ${date.getFullYear()}`);
+
+  // alternatief: datetimeformat gebruiken
+  let options = {
+    dateStyle: 'full',
+    timeStyle: 'long',
+    timeZone: 'Europe/Amsterdam'
+  };
+  console.log(new Intl.DateTimeFormat('nl-NL', options).format(date));
+}
+
+function reverseString(text) {
+  console.log(text.split('').reverse().join(''));
+}
+
+function isTheAnswerToLife(numbers) {
+  if (numbers.includes(42)) {
+    return true;
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+
+  return sum === 42;
+}
+
+function daysUntilMyBirthday(date) {
+  const today = new Date();
+  const birthday = new Date(date);
+
+  const timeDifference = birthday - today;
+  return Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+}
+
+showDate(); //14 februari 2024
+reverseString('hello'); //olleh
+reverseString('antwan'); //nawtna
+console.log(isTheAnswerToLife([3, 15, 23, 1])); //true
+console.log(isTheAnswerToLife([3, 15, 23, 2])); //false
+console.log(isTheAnswerToLife([3, 15, 23, 1000, 42])); //true
+console.log(daysUntilMyBirthday('2026-05-10')); //85
+
+// taal formatting
+const df = new Intl.RelativeTimeFormat('nl-NL', {style: 'long'});
+console.log(df.format(daysUntilMyBirthday('2026-05-10'), 'days')); // "over X dagen"
